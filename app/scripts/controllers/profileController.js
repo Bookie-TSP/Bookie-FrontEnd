802c3080309efe0b8daabfc15e206a758083e5ca
Varis <win3455@hotmail.com>
2015-10-04 14:14:15 +0700

    Test commit

---
+++ app/scripts/controllers/profileController.js
@@ -0,0 +1,31 @@
+angular.module('todo', [])
+	.controller('profile', ['$scope',
+function ($s) {
+	$s.data = [
+        {
+            id: 1,
+            email: 'win3455@hotmail.com',
+            firstName: 'Varis',
+            lastName: 'Kritpolchai',
+            defaultAddressID: '42/39',
+            phoneNumber: '0909839367',
+            identificationNumber: '1101700175626',
+            gender: 'M',
+            birthDate: '15/07/95'
+        },
+        {
+            id: 2,
+            email: 'so_sad_5@hotmail.com',
+            firstName: 'Virus',
+            lastName: 'Computer',
+            defaultAddressID: '12/345',
+            phoneNumber: '0987654321',
+            identificationNumber: '1234567890555',
+            gender: 'F',
+            birthDate: '01/01/99'
+        }
+    ];
+    $s.test = function() {
+    	console.log("test");
+    }
+}]);
