2a3663a860cd037de045139a34f94e05c9fb72f3
Varis <win3455@hotmail.com>
2015-10-05 16:38:28 +0700

    Complete GET data and import css styles

--- app/scripts/controllers/profileController.js
+++ app/scripts/controllers/profileController.js
@@ -1,32 +1,30 @@
 angular.module('todo', [])
 	.controller('profileCtrl', ['$scope', '$http',
-function ($s) {
-	$s.data = [
-        {
-            id: 1,
-            email: 'win3455@hotmail.com',
-            firstName: 'Varis',
-            lastName: 'Kritpolchai',
-            defaultAddressID: '42/39',
-            phoneNumber: '0909839367',
-            identificationNumber: '1101700175626',
-            gender: 'Male',
-            birthDate: '15/07/95'
-        },
-        {
-            id: 2,
-            email: 'so_sad_5@hotmail.com',
-            firstName: 'Virus',
-            lastName: 'Computer',
-            defaultAddressID: '12/345',
-            phoneNumber: '0987654321',
-            identificationNumber: '1234567890555',
-            gender: 'Female',
-            birthDate: '01/01/99'
-        }
-    ];
-    $s.testing = "TEST";
-    $s.test = function() {
-    	console.log("test");
+function ($scope, $http) {
+	$scope.profileData = {};
+    $scope.login = function(){
+    	$http.post('https://bookieservice.herokuapp.com/api/sessions',{
+    		email: "bookie@ku.th",
+    		password: "12345678"
+    	})
+    	.success(function(data){
+    		console.log(JSON.stringify(data));
+    		console.log(data);
+    	}).error(function(data){
+    		console.log(JSON.stringify(data));
+    	});
+    };
+    $scope.getProfile = function() {
+    	var config = {headers: {
+            'Authorization': "UsUz-qtXaxcLGWf-_aaw"
+      	}};
+      	$http.get('https://bookieservice.herokuapp.com/api/myprofile', config)
+      	.success(function(data){
+      		$scope.profileData = data;
+      		console.log(JSON.stringify(data));
+      		console.log(data);
+      	}).error(function(data){
+      		console.log(JSON.stringify(data));
+      	});
     }
 }]);
