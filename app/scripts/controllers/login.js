f22b54fc61f47e5467c34fca131a830f12af4518
Mintra <mntrqq@gmail.com>
2015-10-05 15:52:43 +0700

    show login validation

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -3,6 +3,11 @@ var app = angular.module('app', []);
 app.controller('loginCtrl',['$scope','$http','$window',
   function($scope, $http, $window){
     $scope.auth = "";
+  	$scope.validation = "";
+    setValidation = function(s){
+    	$scope.validation = s;
+    };
+
     $scope.login = function(){
       $http.post('https://bookieservice.herokuapp.com/api/sessions',{
         email: $scope.email,
@@ -14,6 +19,7 @@ app.controller('loginCtrl',['$scope','$http','$window',
         $window.location.href = '../index.html';
       }).error(function(data){
         console.log(data);
+        setValidation("Invalid email or password");
       });
     };
 }]);
