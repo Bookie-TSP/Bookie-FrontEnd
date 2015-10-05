02de511913713ea857fc6d980db17ec625da3070
naneen <neen_tta@hotmail.com>
2015-10-05 16:20:20 +0700

    Started work on automated test

---
+++ test/protractor/specLogin.js
@@ -0,0 +1,10 @@
+describe('Test Login Bookie App', function() {
+    beforeEach(function() {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+    });
+
+    it("and so is a spec", function() {
+        a = true;
+        expect(a).toBe(true);
+    });
+});
