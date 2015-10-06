e713cbfbbf6e97d82b28a8537b11b81f381ed5dd
tan_dooky <tan_dooky@windowslive.com>
2015-10-06 17:08:47 +0700

    add gulp css

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -9,7 +9,14 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./scripts/'));
 });
 
-gulp.task('default', ['scripts'], function() {
+gulp.task('styles', function() {
+  return gulp.src(['./styles/assets/*.css'])
+    .pipe(concatCss('styles.css'))
+    .pipe(gulp.dest('./styles/'));
+});
+
+gulp.task('default', ['scripts', 'styles'], function() {
     server.listen( { path: './server.js' } );
     gulp.watch(['./scripts/application.js', './scripts/controllers/*.js'], ['scripts']);
+    gulp.watch(['./styles/assets/*.css'], ['styles']);
 });
