20cb6405c6405471380f85b63b88726d128fe145
Nathakorn <korn.za@windowslive.com>
2015-10-07 11:16:58 +0700

    edit register form and seperate in 2 posts

--- app/scripts/controllers/register.js
+++ app/scripts/controllers/register.js
@@ -10,27 +10,41 @@ var app = angular.module('register', [])
             $s.auth = "";
             $s.submit = function () {
                 /*
-                console.log($s.firstname);
-                console.log($s.surname);
-                console.log($s.gender);
-                console.log($s.birthDate);
-                console.log($s.IdNo);
-                console.log($s.phoneNo);
                 console.log($s.email);
-                console.log($s.username);
                 console.log($s.password);
-                console.log($s.confirmPassword);
+                console.log($s.password_confirmation);
+                console.log($s.first_name);
+                console.log($s.last_name);
+                console.log($s.birth_date);
+                console.log($s.phone_number);
+                console.log($s.identification_number);
+                console.log($s.latitude);
+                console.log($s.longtitude);
+                 console.log($s.information);
                 */
+                var member = [{
+                    email: email,
+                    password: password,
+                    first_name: first_name,
+                    last_name: last_name,
+                    birth_date: birth_date,
+                    phone_number: phone_number,
+                    identification_number:  identification_number
+                }];
+                var address = [{
+                    first_name: first_name,
+                    last_name: last_name,
+                    latitude: latitude,
+                    longitude: longtitude,
+                    information: information
+                }]
+                //send member
                 $http.post('https://bookieservice.herokuapp.com/api/members',{
-                    first_name: $s.firstname,
-                    sur_name: $s.surname,
-                    gender: $s.gender,
-                    birth_date: $s.birthDate,
-                    identification_number: $s.IdNo,
-                    phone_number: $s.phoneNo,
-                    email: $s.email,
-                    username: $s.username,
-                    password: $s.password
+                   member: member
+                })
+                //send member address
+                $http.post('https://bookieservice.herokuapp.com/api/members',{
+                    member: address
                 })
                 .success(function(data){
                     console.log(JSON.stringify(data));
