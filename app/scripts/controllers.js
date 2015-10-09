064377a788e1b2308ce2011fc449db23973216db
Nathakorn <korn.za@windowslive.com>
2015-10-09 19:20:20 +0700

    show google map

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -33,6 +33,7 @@ app.factory('authFactory', function ($http, $rootScope, $localStorage) {
     };
 });
 
+
 var app = angular.module('app');
 
 app.controller('homeCtrl',['$scope','$http', '$state', '$rootScope',
@@ -65,6 +66,133 @@ app.controller('loginCtrl',['$scope','$http','$state', 'authFactory',
     };
 }]);
 
+function initialize() {
+    var geocoder = new google.maps.Geocoder;
+    var infowindow = new google.maps.InfoWindow;
+
+    var bkk = new google.maps.LatLng(13.7522222, 100.4938889);
+    var mapProp = {
+        center: bkk,
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true
+    };
+
+    var marker = new google.maps.Marker({
+        position: bkk,
+        draggable: true
+    });
+
+    var styles = [
+        {
+            "featureType": "landscape",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "poi",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "lightness": 57
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "lightness": 24
+                }
+            ]
+        },
+        {
+            "featureType": "road",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "transit",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        },
+        {
+            "featureType": "water",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+        }
+    ]
+
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    marker.setMap(map);
+    map.setOptions({styles: styles});
+
+    google.maps.event.addListener(marker, 'dragend', function(mark){
+        var latitude = mark.latLng.lat();
+        var longitude = mark.latLng.lng();
+        geocodeLatLng(geocoder, map, marker, infowindow, latitude, longitude);
+
+        document.getElementById('current').innerHTML = '<p>Marker dropped: Current Lat: ' + latitude.toFixed(3) + ' Current Lng: ' + latitude.toFixed(3) + '</p>';
+    });
+}
+
+// this function is used for convert lat and lng into address
+function geocodeLatLng(geocoder, map, marker, infowindow, latitude, longitude) {
+    var latlng = {
+        lat: latitude,
+        lng: longitude
+    };
+
+    geocoder.geocode({'location': latlng}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            if (results[1]) {
+                var address = results[1].formatted_address;
+                infowindow.setContent(address);
+                infowindow.open(map, marker);
+
+                document.getElementById('address').innerHTML = '<p>Current add: ' + address + '</p>';
+            }
+            else {
+                window.alert('No results found');
+            }
+        }
+        else {
+            window.alert('Geocoder failed due to: ' + status);
+        }
+    });
+}
+
+// initial map and pin by calling initialize function
+google.maps.event.addDomListener(window, 'load', initialize);
 var app = angular.module('app');
 
 app.controller('navCtrl',['$scope','$http', '$state', 'authFactory', '$rootScope',
