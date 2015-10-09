6a5a68ea2b5e74b9125662cca3e2f4081cc7285c
Nathakorn <korn.za@windowslive.com>
2015-10-10 02:02:50 +0700

    edit attribute to post and add more function

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -267,54 +267,62 @@ function ($scope, $http) {
  */
 var app = angular.module('app')
     app.controller('registerCtrl', ['$scope','$http', 'todoApi',
-        function ($s, $http,$factory) {
-            $s.auth = "";
-            $s.submit = function () {
+        function ($scope, $http,$factory) {
+            $scope.auth = "";
+            $scope.submit = function () {
+                console.log($scope.email);
+                console.log($scope.password);
+                console.log($scope.password_confirmation);
+                console.log($scope.first_name);
+                console.log($scope.last_name);
+                console.log($scope.day_birth);
+                console.log($scope.month_birth);
+                console.log($scope.year_birth);
+                console.log($scope.more_info);
+                console.log($scope.phone_number);
+                console.log($scope.identification_number);
+                console.log($scope.latitude);
+                console.log($scope.longtitude);
 
-                console.log($s.email);
-                console.log($s.password);
-                console.log($s.password_confirmation);
-                console.log($s.first_name);
-                console.log($s.last_name);
-                console.log($s.birth_date);
-                console.log($s.phone_number);
-                console.log($s.identification_number);
-                //console.log($s.latitude);
-                //console.log($s.longtitude);
-                //console.log($s.information);
-
-                var member = [{
-                    email: email,
-                    password: password,
-                    first_name: first_name,
-                    last_name: last_name,
-                    birth_date: birth_date,
-                    phone_number: phone_number,
-                    identification_number:  identification_number
-                }];
-                var address = [{
-                    first_name: first_name,
-                    last_name: last_name,
-                    latitude: latitude,
-                    longitude: longtitude,
-                    information: information
-                }]
-                //send member
-                $http.post('https://bookieservice.herokuapp.com/api/members',{
-                   member: member
-                })
-                //send member address
-                $http.post('https://bookieservice.herokuapp.com/api/members',{
-                    member: address
-                })
-                .success(function(data){
-                    console.log(JSON.stringify(data));
-                    console.log(data);
-                    $s.auth = data.auth_token;
-                }).error(function(data){
-                    console.log(JSON.stringify(data));
-                });
-            };
+                if (!$scope.agreeTerm) {
+                    alert("Please agree the term of condition");
+                } else {
+                    var member = [{
+                        email: $scope.email,
+                        password: $scope.password,
+                        first_name: $scope.first_name,
+                        last_name: $scope.last_name,
+                        day_birth: $scope.day_birth,
+                        month_birth: $scope.month_birth,
+                        year_birth: $scope.year_birth,
+                        phone_number: $scope.phone_number,
+                        identification_number: $scope.identification_number
+                    }];
+                    var address = [{
+                        first_name: $scope.first_name,
+                        last_name: $scope.last_name,
+                        latitude: $scope.latitude,
+                        longitude: $scope.longtitude,
+                        more_info: $scope.more_info
+                    }]
+                    //send member
+                    $http.post('https://bookieservice.herokuapp.com/api/members', {
+                        member: member
+                    })
+                    //send member address
+                    $http.post('https://bookieservice.herokuapp.com/api/members', {
+                        member: address
+                    })
+                        .success(function (data) {
+                            console.log(JSON.stringify(data));
+                            console.log(data);
+                            $scope.auth = data.auth_token;
+                        }).error(function (data) {
+                            console.log(JSON.stringify(data));
+                        });
+                    alert("Thank you for sign up!");
+                }
+            }
         }])
     .factory('todoApi', [function () {
 
