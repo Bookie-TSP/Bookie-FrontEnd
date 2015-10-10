f59b0a8702b21de301a33b73e3ea0f9b453b377f
Varis <win3455@hotmail.com>
2015-10-10 21:46:26 +0700

    Edit Profile (should be) done

--- app/scripts/controllers/editProfileController.js
+++ app/scripts/controllers/editProfileController.js
@@ -18,7 +18,7 @@ angular.module('app', [])
       $scope.getProfile = function() {
           console.log("Getting the profile");
           var config = {headers: {
-            'Authorization': "M8LzsSzLYD-7mCfMgqf7"
+            'Authorization': "Ba9fRyM4svMysRvQaLCV"
         }};
         $http.get('https://bookieservice.herokuapp.com/api/myprofile', config)
         .success(function(data){
@@ -33,32 +33,31 @@ angular.module('app', [])
     $scope.editProfile = function(){
         console.log("Editing the profile");
         var config = {headers: {
-            'Authorization': "M8LzsSzLYD-7mCfMgqf7"
+            'Authorization': "Ba9fRyM4svMysRvQaLCV"
         }};
         // $scope.member.password = "123451234";
         // $scope.member.password_confirmation = "123451234";
         console.log($scope.profileData);
         console.log("Password: " + $scope.profileData.password);
         $scope.email = $scope.profileData.email
-        $http.put('https://bookieservice.herokuapp.com/api/members/1', {
+        $http.put('https://bookieservice.herokuapp.com/api/members', {
             member: 
                 {
                     email: $scope.profileData.email,
-                    password: "12340000",
-                    password_confirmation: "12340000",
-                    first_name: "Varis",
-                    last_name: "Kritpolchai",
-                    phone_number: "0909839367",
-                    identification_number: "1101700175626",
-                    gender: "Male",
-                    birth_date: "15/07/95"
+                    password: $scope.profileData.password,
+                    password_confirmation: $scope.profileData.password,
+                    first_name: $scope.profileData.first_name,
+                    last_name: $scope.profileData.last_name,
+                    phone_number: $scope.profileData.phone_number,
+                    identification_number: $scope.profileData.identification_number,
+                    gender: $scope.profileData.gender,
+                    birth_date: $scope.profileData.birth_date
                 }
             },config)
       .success(function(data){
         console.log("Success");
         console.log(JSON.stringify(data));
         console.log(data);
-        $scope.getProfile();
     }).error(function(data){
         console.log("Error");
         console.log(JSON.stringify(data));
