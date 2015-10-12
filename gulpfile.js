cd614baa91cff35f56df743b6492f07797ccf91f
tan_dooky <tan_dooky@windowslive.com>
2015-10-12 17:35:17 +0700

    done merge master

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,20 @@ var server = require( 'gulp-develop-server' );
 var browserSync = require('browser-sync').create();
 
 gulp.task('scripts', function() {
-  return gulp.src(['./scripts/application.js', './scripts/controllers/*.js', './scripts/factories/*.js'])
-    .pipe(concat('controllers.js'))
->>>>>>> master:app/gulpfile.js
-    .pipe(gulp.dest('./scripts/'));
+  return gulp.src(['app/scripts/application.js', 'app/scripts/controllers/*.js', 'app/scripts/factories/*.js'])
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('app/scripts/'));
 });
 
 gulp.task('styles', function() {
-  return gulp.src(['./styles/assets/*.css'])
+  return gulp.src(['app/styles/assets/*.css'])
     .pipe(concatCss('styles.css'))
-    .pipe(gulp.dest('./styles/'));
+    .pipe(gulp.dest('app/styles/'));
 });
 
 gulp.task('default', ['scripts', 'styles'], function() {
-    gulp.watch(['./scripts/application.js', './scripts/controllers/*.js'], ['scripts']);
-    gulp.watch(['./styles/assets/*.css'], ['styles']);
+    gulp.watch(['app/scripts/application.js', 'app/scripts/controllers/*.js', 'app/scripts/factories/*.js'], ['scripts']);
+    gulp.watch(['app/styles/assets/*.css'], ['styles']);
     browserSync.init({
         port: 8000,
         server: {
