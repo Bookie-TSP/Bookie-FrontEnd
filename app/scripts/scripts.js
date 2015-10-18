00f04c9ebc0c0fb61e7d6f08788c5fc2d5b11dbe
Nathakorn <korn.za@windowslive.com>
2015-10-18 20:59:02 +0700

    add container and seperate row

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -40,111 +40,10 @@ function ($rootScope, $state, $stateParams) {
   $rootScope.$stateParams = $stateParams;
 }]);
 
-app.controller('editProfileCtrl', ['$scope', '$http', 'googleMap', 'authFactory', '$q', '$state',
-	function ($scope, $http, googleMap, authFactory, $q, $state) {
-		if (authFactory.getAuth() === undefined) {
-			$state.go("home");
-		}
-
-		$scope.getProfile = function () {
-			console.log("Getting the profile");
-			var config = {
-				headers: {
-					'Authorization': authFactory.getAuth()
-				}
-			};
-			var birth = "";
-			$q.all([
-					$http.get('https://bookieservice.herokuapp.com/api/myprofile', config)
-					.success(function (data) {
-						$scope.profileData = data;
-						authFactory.setMember(data);
-						$scope.profileData = authFactory.getMember();
-						console.log(data);
-					})
-					.error(function (data) {
-						console.log(data);
-					})
-			])
-			.then(function () {
-				$scope.setDate();
-				$state.go("viewProfile");
-			});
-		};
-
-		$scope.setDate = function () {
-			if($scope.profileData.birth_date !== null){
-				birth = $scope.profileData.birth_date.split("-");
-				$scope.date = birth[2];
-				$scope.month = birth[1];
-				$scope.year = birth[0];
-			}
-		};
-
-		$scope.editProfile = function () {
-			console.log("Editing the profile");
-			var config = {
-				headers: {
-					'Authorization': authFactory.getAuth()
-				}
-			};
-			var birth_date = $scope.date + "/" + $scope.month + "/" + $scope.year;
-			$http.put('https://bookieservice.herokuapp.com/api/members', {
-					member: {
-						email: $scope.profileData.email,
-						password: $scope.profileData.password,
-						password_confirmation: $scope.profileData.password,
-						first_name: $scope.profileData.first_name,
-						last_name: $scope.profileData.last_name,
-						phone_number: $scope.profileData.phone_number,
-						identification_number: $scope.profileData.identification_number,
-						gender: $scope.profileData.gender,
-						birth_date: birth_date
-					}
-				}, config)
-				.success(function (data) {
-					$scope.profileData.password = "";
-					$scope.getProfile();
-					console.log(data);
-				})
-				.error(function (data) {
-					console.log(data);
-				});
-		};
-
-		$scope.initial = function () {
-			$scope.profileData = authFactory.getMember();
-			$scope.setDate();
-		};
-
-		$scope.initial();
-	}
-]);
-
-app.controller('homeCtrl',['$scope', '$http', '$state', '$rootScope',
+app.controller('homeCtrl',['$scope','$http', '$state', '$rootScope',
     function($scope, $http, $state, $rootScope){
-    	$scope.bookProfile = function () {
-			$state.go("bookProfile");
-		};
-}]);
 
-app.controller('bookProfileCtrl',['$scope', '$http', '$state', 
-	function($scope, $http, $state, $rootScope){
-		$scope.bookInfo = 
-		{
-			title: "Klee Wyck",
-			isbn: "5-10-142526-5",
-			author: "Emily Carr",
-			language: "English",
-			pages: 315,
-			publisher: "Irwin Publishing"
-		}
-		$scope.addToCart = function() {
-			console.log("Adding to cart");
-			console.log("Sending " + $scope.bookInfo.title + " to the cart");
-		}
-	}
-]);
+}]);
 
 app.controller('loginCtrl', ['$scope', '$http', '$state', 'authFactory',
 	function ($scope, $http, $state, authFactory) {
@@ -270,17 +169,6 @@ app.controller('registerCtrl', ['$scope', '$http', 'googleMap', '$state', 'authF
 						alert("error : " + data.error);
 					});
 			}
-		};
-}]);
-
-app.controller('profileCtrl', ['$scope', '$http', '$state', 'authFactory',
-function ($scope, $http, $state, authFactory) {
-		if (authFactory.getAuth() === undefined) {
-			$state.go("home");
-		}
-		$scope.profileData = authFactory.getMember();
-		$scope.editProfile = function () {
-			$state.go("editProfile");
 		};
 }]);
 
