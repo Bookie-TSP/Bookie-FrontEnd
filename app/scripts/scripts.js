2aa07f63932eb524f10e1cf5b4601b546aa116bc
Varis <win3455@hotmail.com>
2015-10-19 22:53:14 +0700

    Most of the thing in the page are done, also the variables for API are ready, but still working on the auth to only show the shipping and places only when already logged in

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -42,14 +42,66 @@ function ($rootScope, $state, $stateParams) {
 
 app.controller('bookProfileCtrl', ['$scope', '$http', '$state', 'googleMap', 'authFactory',
 	function ($scope, $http, $state, googleMap, authFactory) {
-		// if (authFactory.getAuth() !== undefined) {
+		$scope.loggedIn = false;
+		if (authFactory.getAuth() !== undefined) {
 			googleMap.initialize();
-		// }
+			loggedIn = true;
+		}
 		$scope.tempInfo = "TEST";
 		$scope.tabs = [
-    		{ title:'Dynamic Title 1', content:'Dynamic content 1' },
-    		{ title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
-  		];
+    	{ title:'Dynamic Title 1', content:'Dynamic content 1' },
+    	{ title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }];
+  		$scope.newBooksInfo = [
+  		{
+  			price: 84,
+  			quality: "Perfect",
+  			address: "508 Treeview Trail Barneveld, WI 55303",
+  			quantity: 16
+  		},
+  		{
+  			price: 175,
+  			quality: "Perfect",
+  			address: "217 E Division Madison, WI 53666",
+  			quantity: 5
+  		},
+  		{
+  			price: 98,
+  			quality: "Perfect",
+  			address: "505 Sampson Apt 3C",
+  			quantity: 32
+  		},
+  		{
+  			price: 144,
+  			quality: "Perfect",
+  			address: "4204 Military Ridge Rd Dodgeville, WI 53224",
+  			quantity: 0
+  		}];
+  		$scope.oldBooksInfo = [
+  		{
+  			price: 142,
+  			quality: "Good",
+  			address: "619 Maiden St Mineral Point, WI 53444",
+  			quantity: 11
+  		},
+  		{
+  			price: 84,
+  			quality: "Almost perfect",
+  			address: "4770 Main St Dodgeville, WI 53222",
+  			quantity: 5
+  		}];
+  		$scope.rentBooksInfo = [
+  		{
+  			price: 65,
+  			quality: "Perfect",
+  			address: "402 E Park Apt 202B Montfort, WI 53555",
+  			quantity: 0
+  		},
+  		{
+  			price: 92,
+  			quality: "Good",
+  			address: "102 Center St Cobb, WI 53666",
+  			quantity: 10
+  		}];
 		$scope.bookInfo = 
 		{
 			title: "Klee Wyck",
@@ -64,22 +116,6 @@ app.controller('bookProfileCtrl', ['$scope', '$http', '$state', 'googleMap', 'au
 		$scope.addToCart = function () {
 			console.log("Adding " + $scope.bookInfo.title + " to the cart");
 			console.log("The book " + $scope.bookInfo.title + " has been added to the cart.");
-		}
-		$scope.toggleHome = function() {
-			tempInfo = "Home";
-			console.log(tempInfo);
-		}
-		$scope.toggleMenu1 = function() {
-			tempInfo = "Menu1";
-			console.log(tempInfo);
-		}
-		$scope.toggleMenu2 = function() {
-			tempInfo = "Menu2";
-			console.log(tempInfo);
-		}
-		$scope.toggleMenu3 = function() {
-			tempInfo = "Menu3";
-			console.log(tempInfo);
 		}
 	}
 ]);
