e43fc0601439d391d4abb92b3a1356a1d3a8900d
Varis <win3455@hotmail.com>
2015-10-19 23:14:18 +0700

    Added comments to bookProfile.js and bookProfile.html and also implement two buttons to move to the top and bottom of the page and few minor changes in the page

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -40,17 +40,17 @@ function ($rootScope, $state, $stateParams) {
   $rootScope.$stateParams = $stateParams;
 }]);
 
-app.controller('bookProfileCtrl', ['$scope', '$http', '$state', 'googleMap', 'authFactory',
-	function ($scope, $http, $state, googleMap, authFactory) {
+app.controller('bookProfileCtrl', ['$scope', '$http', '$anchorScroll', '$location', '$state', 'googleMap', 'authFactory',
+	function ($scope, $http, $anchorScroll, $location, $state, googleMap, authFactory) {
 		$scope.loggedIn = false;
+
+		// Check whether the Member has logged in or not
 		if (authFactory.getAuth() !== undefined) {
 			googleMap.initialize();
 			loggedIn = true;
 		}
-		$scope.tempInfo = "TEST";
-		$scope.tabs = [
-    	{ title:'Dynamic Title 1', content:'Dynamic content 1' },
-    	{ title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }];
+
+    	// List that contains the price, condition, address, and quantity of the book in each shop for buying
   		$scope.newBooksInfo = [
   		{
   			price: 84,
@@ -76,6 +76,8 @@ app.controller('bookProfileCtrl', ['$scope', '$http', '$state', 'googleMap', 'au
   			address: "4204 Military Ridge Rd Dodgeville, WI 53224",
   			quantity: 0
   		}];
+
+  		// List that contains the price, condition, address, and quantity of the book in each shop for buying
   		$scope.oldBooksInfo = [
   		{
   			price: 142,
@@ -89,6 +91,8 @@ app.controller('bookProfileCtrl', ['$scope', '$http', '$state', 'googleMap', 'au
   			address: "4770 Main St Dodgeville, WI 53222",
   			quantity: 5
   		}];
+
+  		// List that contains the price, condition, address, and quantity of the book in each shop for renting
   		$scope.rentBooksInfo = [
   		{
   			price: 65,
@@ -102,6 +106,8 @@ app.controller('bookProfileCtrl', ['$scope', '$http', '$state', 'googleMap', 'au
   			address: "102 Center St Cobb, WI 53666",
   			quantity: 10
   		}];
+
+  		// Information of the book that has been received
 		$scope.bookInfo = 
 		{
 			title: "Klee Wyck",
@@ -113,9 +119,22 @@ app.controller('bookProfileCtrl', ['$scope', '$http', '$state', 'googleMap', 'au
 			publishedDate: "14/05/1944",
 			description: "Douglas & McIntyre is proud to announce definitive, completely redesigned editions of Emily Carr’s seven enduring classic books. These are beautifully crafted keepsake editions of the literary world of Emily Carr, each with an introduction by a distinguished Canadian writer or authority on Emily Carr and her work."
 		}
+
+		// Use for adding the book to the cart with its details
 		$scope.addToCart = function () {
 			console.log("Adding " + $scope.bookInfo.title + " to the cart");
 			console.log("The book " + $scope.bookInfo.title + " has been added to the cart.");
+		}
+
+		$scope.moveToTop = function() {
+			$location.hash('top');
+			$anchorScroll();
+		}
+
+		// Use for scrolling the page to bottom
+		$scope.moveToBottom = function() {
+			$location.hash('bottom');
+			$anchorScroll();
 		}
 	}
 ]);
