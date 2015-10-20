9392ff2dc31b7b6c8017e8c8f0503d0ca8515ca1
tan_dooky <tan_dooky@windowslive.com>
2015-10-20 20:14:29 +0700

    delete file from another branch

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,6 +1,3 @@
 app.controller('homeCtrl',['$scope','$http', '$state', '$rootScope',
     function($scope, $http, $state, $rootScope){
-    	$scope.bookProfile = function() {
-    		$state.go("bookProfile");
-    	}
 }]);
