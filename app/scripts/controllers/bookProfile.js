03cdc1eec133c86e33e80525ae5245c2e385a051
Varis <win3455@hotmail.com>
2015-10-21 12:00:16 +0700

    Fix the Book Information to arrage in columns and rows and also check whether the variable is null or not

--- app/scripts/controllers/bookProfile.js
+++ app/scripts/controllers/bookProfile.js
@@ -1,188 +1,193 @@
 app.controller('bookProfileCtrl', ['$scope', '$http', '$anchorScroll', '$location', '$state', 'googleMap', 'authFactory',
-  function ($scope, $http, $anchorScroll, $location, $state, googleMap, authFactory) {
-    $scope.loggedIn = false;
-    googleMap.initialize();
-    // Check whether the Member has logged in or not
-    if (authFactory.getAuth() !== undefined) {
-      loggedIn = true;
+    function ($scope, $http, $anchorScroll, $location, $state, googleMap, authFactory) {
+        $scope.loggedIn = false;
+        googleMap.initialize();
+        // Check whether the Member has logged in or not
+        if (authFactory.getAuth() !== undefined) {
+            loggedIn = true;
+        }
+
+        // List that contains the price, condition, address, and quantity of the book in each shop for buying
+        $scope.newBooksInfo = [
+        {
+            price: 84,
+            condition: "Perfect",
+            address: "508 Treeview Trail Barneveld, WI 55303",
+            quantity: 16
+        },
+        {
+            price: 175,
+            condition: "Perfect",
+            address: "217 E Division Madison, WI 53666",
+            quantity: 32
+        },
+        {
+            price: 98,
+            condition: "Perfect",
+            address: "505 Sampson Apt 3C",
+            quantity: 0
+        },
+        {
+            price: 144,
+            condition: "Perfect",
+            address: "4204 Military Ridge Rd Dodgeville, WI 53224",
+            quantity: 0
+        },
+        {
+            price: 175,
+            condition: "Perfect",
+            address: "217 E Division Madison, WI 53666",
+            quantity: 59
+        },
+        {
+            price: 98,
+            condition: "Perfect",
+            address: "505 Sampson Apt 3C",
+            quantity: 0
+        },
+        {
+            price: 175,
+            condition: "Perfect",
+            address: "217 E Division Madison, WI 53666",
+            quantity: 5
+        },
+        {
+            price: 98,
+            condition: "Perfect",
+            address: "505 Sampson Apt 3C",
+            quantity: 123
+        }];
+
+        // List that contains the price, condition, address, and quantity of the book in each shop for buying
+        $scope.oldBooksInfo = [
+        {
+            price: 142,
+            condition: "Good",
+            address: "619 Maiden St Mineral Point, WI 53444",
+            quantity: 11
+        },
+        {
+            price: 62,
+            condition: "Pretty Good",
+            address: "941 Crystal St Mirana Point, WI 4145",
+            quantity: 0
+        },
+        {
+            price: 198,
+            condition: "Perfect",
+            address: "505 Sampson Apt 3C",
+            quantity: 23
+        },
+        {
+            price: 62,
+            condition: "Pretty Good",
+            address: "941 Crystal St Mirana Point, WI 4145",
+            quantity: 0
+        },
+        {
+            price: 198,
+            condition: "Perfect",
+            address: "505 Sampson Apt 3C",
+            quantity: 23
+        },
+        {
+            price: 84,
+            condition: "Almost Perfect",
+            address: "4770 Main St Dodgeville, WI 53222",
+            quantity: 5
+        }];
+
+        // List that contains the price, condition, address, and quantity of the book in each shop for renting
+        $scope.rentBooksInfo = [
+        {
+            price: 65,
+            condition: "Perfect",
+            address: "402 E Park Apt 202B Montfort, WI 53555",
+            quantity: 0
+        },
+        {
+            price: 98,
+            condition: "Very Good",
+            address: "9910 High Hill Smd 941A",
+            quantity: 0
+        },
+        {
+            price: 175,
+            condition: "Very Good",
+            address: "217 E Division Madison, WI 53666",
+            quantity: 5
+        },
+        {
+            price: 90,
+            condition: "Good",
+            address: "505 Sampson Apt 3C",
+            quantity: 32
+        },
+        {
+            price: 175,
+            condition: "Bad",
+            address: "217 E Division Madison, WI 53666",
+            quantity: 0
+        },
+        {
+            price: 94,
+            condition: "Very Good",
+            address: "505 Sampson Apt 3C",
+            quantity: 36
+        },
+        {
+            price: 175,
+            condition: "Perfect",
+            address: "217 E Division Madison, WI 53666",
+            quantity: 5
+        },
+        {
+            price: 98,
+            condition: "Almost Perfect",
+            address: "505 Sampson Apt 3C",
+            quantity: 0
+        },
+        {
+            price: 176,
+            condition: "Perfect",
+            address: "505 Sampson Apt 3C",
+            quantity: 91
+        },
+        {
+            price: 92,
+            condition: "Good",
+            address: "102 Center St Cobb, WI 53666",
+            quantity: 10
+        }];
+
+        // Define bookInfo
+        $scope.bookInfo = {};
+
+        // Get information of the book from the API
+        $scope.getBookProfile = function() {
+            $http.get('https://bookieservice.herokuapp.com/api/books/1')
+            .success(function (data) {
+                console.log(data);
+                $scope.bookInfo = data;
+            })
+            .error(function (data) {
+                console.log(data);
+            })
+        }
+
+        // Call getBookProfile()
+        $scope.getBookProfile();
+
+        // Use for adding the book to the cart with its details
+        $scope.addToCart = function () {
+            console.log("Adding " + $scope.bookInfo.title + " to the cart");
+            console.log("The book " + $scope.bookInfo.title + " has been added to the cart.");
+        }
+
+        // Use for scrolling the page to bottom
+        $scope.moveToBottom = function() {
+            $location.hash('bottom');
+            $anchorScroll();
+        }
     }
-
-      // List that contains the price, condition, address, and quantity of the book in each shop for buying
-      $scope.newBooksInfo = [
-      {
-        price: 84,
-        condition: "Perfect",
-        address: "508 Treeview Trail Barneveld, WI 55303",
-        quantity: 16
-      },
-      {
-        price: 175,
-        condition: "Perfect",
-        address: "217 E Division Madison, WI 53666",
-        quantity: 32
-      },
-      {
-        price: 98,
-        condition: "Perfect",
-        address: "505 Sampson Apt 3C",
-        quantity: 0
-      },
-      {
-        price: 144,
-        condition: "Perfect",
-        address: "4204 Military Ridge Rd Dodgeville, WI 53224",
-        quantity: 0
-      },
-      {
-        price: 175,
-        condition: "Perfect",
-        address: "217 E Division Madison, WI 53666",
-        quantity: 59
-      },
-      {
-        price: 98,
-        condition: "Perfect",
-        address: "505 Sampson Apt 3C",
-        quantity: 0
-      },
-      {
-        price: 175,
-        condition: "Perfect",
-        address: "217 E Division Madison, WI 53666",
-        quantity: 5
-      },
-      {
-        price: 98,
-        condition: "Perfect",
-        address: "505 Sampson Apt 3C",
-        quantity: 123
-      }];
-
-      // List that contains the price, condition, address, and quantity of the book in each shop for buying
-      $scope.oldBooksInfo = [
-      {
-        price: 142,
-        condition: "Good",
-        address: "619 Maiden St Mineral Point, WI 53444",
-        quantity: 11
-      },
-      {
-        price: 62,
-        condition: "Pretty Good",
-        address: "941 Crystal St Mirana Point, WI 4145",
-        quantity: 0
-      },
-      {
-        price: 198,
-        condition: "Perfect",
-        address: "505 Sampson Apt 3C",
-        quantity: 23
-      },
-      {
-        price: 62,
-        condition: "Pretty Good",
-        address: "941 Crystal St Mirana Point, WI 4145",
-        quantity: 0
-      },
-      {
-        price: 198,
-        condition: "Perfect",
-        address: "505 Sampson Apt 3C",
-        quantity: 23
-      },
-      {
-        price: 84,
-        condition: "Almost Perfect",
-        address: "4770 Main St Dodgeville, WI 53222",
-        quantity: 5
-      }];
-
-      // List that contains the price, condition, address, and quantity of the book in each shop for renting
-      $scope.rentBooksInfo = [
-      {
-        price: 65,
-        condition: "Perfect",
-        address: "402 E Park Apt 202B Montfort, WI 53555",
-        quantity: 0
-      },
-      {
-        price: 98,
-        condition: "Very Good",
-        address: "9910 High Hill Smd 941A",
-        quantity: 0
-      },
-      {
-        price: 175,
-        condition: "Very Good",
-        address: "217 E Division Madison, WI 53666",
-        quantity: 5
-      },
-      {
-        price: 90,
-        condition: "Good",
-        address: "505 Sampson Apt 3C",
-        quantity: 32
-      },
-      {
-        price: 175,
-        condition: "Bad",
-        address: "217 E Division Madison, WI 53666",
-        quantity: 0
-      },
-      {
-        price: 94,
-        condition: "Very Good",
-        address: "505 Sampson Apt 3C",
-        quantity: 36
-      },
-      {
-        price: 175,
-        condition: "Perfect",
-        address: "217 E Division Madison, WI 53666",
-        quantity: 5
-      },
-      {
-        price: 98,
-        condition: "Almost Perfect",
-        address: "505 Sampson Apt 3C",
-        quantity: 0
-      },
-      {
-        price: 176,
-        condition: "Perfect",
-        address: "505 Sampson Apt 3C",
-        quantity: 91
-      },
-      {
-        price: 92,
-        condition: "Good",
-        address: "102 Center St Cobb, WI 53666",
-        quantity: 10
-      }];
-
-      // Information of the book that has been received
-    $scope.bookInfo = 
-    {
-      title: "Klee Wyck",
-      isbn: "978-1553650256",
-      author: "Emily Carr",
-      language: "English",
-      pages: 144,
-      publisher: "Douglas & McIntyre",
-      publishedDate: "14/05/1944",
-      description: "Douglas & McIntyre is proud to announce definitive, completely redesigned editions of Emily Carr’s seven enduring classic books. These are beautifully crafted keepsake editions of the literary world of Emily Carr, each with an introduction by a distinguished Canadian writer or authority on Emily Carr and her work."
-    }
-
-    // Use for adding the book to the cart with its details
-    $scope.addToCart = function () {
-      console.log("Adding " + $scope.bookInfo.title + " to the cart");
-      console.log("The book " + $scope.bookInfo.title + " has been added to the cart.");
-    }
-
-    // Use for scrolling the page to bottom
-    $scope.moveToBottom = function() {
-      $location.hash('bottom');
-      $anchorScroll();
-    }
-  }
 ]);
