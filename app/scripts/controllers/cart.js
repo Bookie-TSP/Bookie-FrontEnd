fb134e3fa9ed558fa87728f80fc797c566bcc08f
rookiemo <frozenprince.k@gmail.com>
2015-10-24 21:57:58 +0700

    fixing cart

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -1,4 +1,17 @@
 app.controller('cartCtrl',['$scope','$http', '$state',
-    function($scope, $http, $state){
-
+    function ($scope, $http, $state, authFactory){
+		if (authFactory.getAuth() !== undefined) {
+			$state.go("home");
+		}
+		$scope.getCart = function() {
+            $http.get('https://bookieservice.herokuapp.com/api/members/cart/show',config)
+            .success(function (data) {
+                console.log(data);
+                $scope.stocks = data.stocks;
+            })
+            .error(function (data) {
+                console.log(data);
+            })
+        };
+        $scope.getCart();
 }]);
--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -41,10 +41,22 @@ function ($rootScope, $state, $stateParams) {
 }]);
 
 app.controller('cartCtrl',['$scope','$http', '$state',
-    function($scope, $http, $state){
-
+    function ($scope, $http, $state, authFactory){
+		if (authFactory.getAuth() !== undefined) {
+			$state.go("home");
+		}
+		$scope.getCart = function() {
+            $http.get('https://bookieservice.herokuapp.com/api/members/cart/show',config)
+            .success(function (data) {
+                console.log(data);
+                $scope.stocks = data.stocks;
+            })
+            .error(function (data) {
+                console.log(data);
+            })
+        };
+        $scope.getCart();
 }]);
-
 app.controller('editProfileCtrl', ['$scope', '$http', 'googleMap', 'authFactory', '$q', '$state',
 	function ($scope, $http, googleMap, authFactory, $q, $state) {
 		if (authFactory.getAuth() === undefined) {
