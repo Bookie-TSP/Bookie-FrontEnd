505914787f510b06b84d47e774264ebef646a947
tan_dooky <tan_dooky@windowslive.com>
2015-11-07 22:19:23 +0700

    remove botton

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -42,23 +42,37 @@ function ($rootScope, $state, $stateParams) {
 
 app.controller('cartCtrl',['$scope','$http', '$state', 'authFactory',
     function ($scope, $http, $state, authFactory){
+        var config = {
+            headers: {
+                'Authorization': authFactory.getAuth()
+            }
+        };
 		$scope.getCart = function() {
-            var config = {
-                headers: {
-                    'Authorization': authFactory.getAuth()
-                }
-            };
             $http.get('https://bookieservice.herokuapp.com/api/members/cart/show',config)
             .success(function (data) {
                 console.log(data);
-                $scope.stocks = data.stocks;
+                $scope.cart = data;
+                $scope.stocks = $scope.cart.stocks;
             })
             .error(function (data) {
                 console.log(data);
-            })
+            });
         };
+        // $scope.removeStock = function(id) {
+        //     $http.post('https://bookieservice.herokuapp.com/api/members/cart/remove',{
+        //         stock_id: id
+        //     },config)
+        //     .success(function(data){
+        //         $scope.cart = data;
+        //         $scope.stocks = $scope.cart.stocks;
+        //     })
+        //     .error(function(data){
+        //         console.log(data);
+        //     });
+        // };
         $scope.getCart();
 }]);
+
 app.controller('editProfileCtrl', ['$scope', '$http', 'googleMap', 'authFactory', '$q', '$state',
 	function ($scope, $http, googleMap, authFactory, $q, $state) {
 		if (authFactory.getAuth() === undefined) {
