47493c47017389a10a4ce5b504ee96784ac9f27b
tan_dooky <tan_dooky@windowslive.com>
2015-11-10 15:23:08 +0700

    cancel item

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -5,33 +5,39 @@ app.controller('cartCtrl',['$scope','$http', '$state', 'authFactory',
                 'Authorization': authFactory.getAuth()
             }
         };
+        $scope.getTotal = function() {
+            $scope.total = 0;
+            for(var i = 0, len = $scope.stocks.length; i < len; i++) {
+                $scope.total += $scope.stocks[i].price;
+            }
+        };
 		$scope.getCart = function() {
             $http.get('https://bookieservice.herokuapp.com/api/members/cart/show',config)
             .success(function (data) {
                 console.log(data);
                 $scope.cart = data;
                 $scope.stocks = $scope.cart.stocks;
-                $scope.total = 0;
-                    for(var i = 0, len = $scope.stocks.length; i < len; i++) {
-                        $scope.total += $scope.stocks[i].price;
-                    }
-                console.log($scope.total)
+                $scope.getTotal();
+                console.log($scope.total);
             })
             .error(function (data) {
                 console.log(data);
             });
         };
-        // $scope.removeStock = function(id) {
-        //     $http.post('https://bookieservice.herokuapp.com/api/members/cart/remove',{
-        //         stock_id: id
-        //     },config)
-        //     .success(function(data){
-        //         $scope.cart = data;
-        //         $scope.stocks = $scope.cart.stocks;
-        //     })
-        //     .error(function(data){
-        //         console.log(data);
-        //     });
-        // };
+        $scope.removeStock = function(id) {
+            console.log(id);
+            $http.post('https://bookieservice.herokuapp.com/api/members/cart/remove',{
+                stock: {
+                    stock_id: id
+                }
+            },config)
+            .success(function(data){
+                $scope.cart = data;
+                $scope.stocks = $scope.cart.stocks;
+            })
+            .error(function(data){
+                console.log(data);
+            });
+        };
         $scope.getCart();
 }]);
