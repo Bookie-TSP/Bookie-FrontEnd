f2c0938e644eb36c778681dcb8154d90ac5f1e38
Varis <win3455@hotmail.com>
2015-11-11 17:16:23 +0700

    Completed pager function for tabs (new book, used book, rent book)

--- app/scripts/controllers/bookProfile.js
+++ app/scripts/controllers/bookProfile.js
@@ -2,12 +2,23 @@ app.controller('bookProfileCtrl', ['$scope', '$http', '$anchorScroll', '$locatio
     function ($scope, $http, $anchorScroll, $location, $state, authFactory) {
         $scope.loggedIn = false;
 
-        // Variables for Pagers (still developing)
+        // Tab array of stocks
+        $scope.buyNewBook = [];
+        $scope.buyUsedBook = [];
+        $scope.rentBook = [];
+
+        // Max showing page for Pagers
         $scope.maxSize = 5;
-        $scope.bigTotalItems = 175;
-        $scope.bigCurrentPage = 1;
-        $scope.totalItems = 64;
-        $scope.currentPage = 4;
+
+        // Initialize pager variables for total items in each tab
+        $scope.buyNewBookTotalItems;
+        $scope.buyUsedBookTotalItems;
+        $scope.rentBookTotalItems;
+
+        // Initialize pager variables for the current page of each tab
+        $scope.buyNewBookCurrentPage = 1;
+        $scope.buyUsedBookCurrentPage = 1;
+        $scope.rentBookCurrentPage = 1;
 
         // Initialize Google Map from the mapFactory.js
         //googleMap.initialize();
@@ -26,23 +37,55 @@ app.controller('bookProfileCtrl', ['$scope', '$http', '$anchorScroll', '$locatio
             .success(function (data) {
                 console.log(data);
                 $scope.bookInfo = data;
+                $scope.seperate();
+                $scope.setPagerTotalItems();
             })
             .error(function (data) {
                 console.log(data);
             })
         }
 
+        // Seperate books into categories
+        $scope.seperate = function() {
+            for (var i = 0; i < $scope.bookInfo.line_stocks.length; i++) {
+                if ($scope.bookInfo.line_stocks[i].stocks[0].type === 'sell') {
+                    if ($scope.bookInfo.line_stocks[i].stocks[0].condition === 'new') {
+                        $scope.buyNewBook.push($scope.bookInfo.line_stocks[i]);
+                    }
+                    else if ($scope.bookInfo.line_stocks[i].stocks[0].condition === 'used') {
+                        $scope.buyUsedBook.push($scope.bookInfo.line_stocks[i]);
+                    }
+                }
+                else if ($scope.bookInfo.line_stocks[i].stocks[0].type === 'lend') {
+                    $scope.rentBook.push($scope.bookInfo.line_stocks[i]);
+                }
+            }
+        }
+
+        $scope.setPagerTotalItems = function() {
+            $scope.buyNewBookTotalItems = $scope.buyNewBook.length * 2;
+            $scope.buyUsedBookTotalItems = $scope.buyUsedBook.length * 2;
+            $scope.rentBookTotalItems = $scope.rentBook.length * 2;
+        }
+
         // Call getBookProfile()
         $scope.getBookProfile();
 
         // Use for adding the book to the cart with its details
-        $scope.addToCart = function (line_stock) {
-            console.log("Adding the book that costs $" + line_stock.stocks[0].price + " to the cart");
-            $http.post('https://bookieservice.herokuapp.com/api/members/cart/add', {
-                stocks: {
-                    stock_id: 1
+        $scope.addToCart = function(line_stock) {
+            var stockRandomizer = Math.floor(Math.random() * (line_stock.stocks.length + 1));
+            var config = {
+                headers: {
+                    'Authorization': authFactory.getAuth()
                 }
-            })
+            };
+            // console.log(line_stock);
+            // console.log("Adding the book that costs $" + line_stock.stocks[0].price + " to the cart");
+            $http.post('https://bookieservice.herokuapp.com/api/members/cart/add', {
+                stock: {
+                    stock_id: line_stock.stocks[stockRandomizer].id
+                }
+            }, config)
             .success(function(data){
                 console.log(JSON.stringify(data));
                 console.log(data);
@@ -51,7 +94,7 @@ app.controller('bookProfileCtrl', ['$scope', '$http', '$anchorScroll', '$locatio
             .error(function(data){
                 console.log(JSON.stringify(data));
             });
-            console.log("The book that costs $" + book.price + " has been added to the cart.");
+            console.log("The book that costs $" + line_stock.stocks[0].price + " has been added to the cart.");
         }
 
         // Use for scrolling the page to bottom
