d1ab3aa00f1c0b7665f7609b72f88d04b0ea1624
Varis <win3455@hotmail.com>
2015-11-13 10:01:46 +0700

    Adjust stuffs to make them look better

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -586,12 +586,14 @@ app.controller('orderCtrl', ['$scope', '$http', '$state', 'authFactory',
 		$scope.pendingContents = [{
 			price: 120,
 			name: "Harry Potter the next gen",
+			orderDate: "12/04/14",
 			status: "Ordered",
 			hasPaid: false
 		},
 		{
 			price: 325,
 			name: "Punpun and friends",
+			orderDate: "12/04/14",
 			status: "Confirmed",
 			hasPaid: true
 		}]
