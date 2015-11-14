c7fbec1b7c59636dbdde8304f1a2891981d0fa3b
Varis <win3455@hotmail.com>
2015-11-14 14:04:29 +0700

    Added accordion to the page

--- app/scripts/controllers/order.js
+++ app/scripts/controllers/order.js
@@ -7,15 +7,43 @@ app.controller('orderCtrl', ['$scope', '$http', '$state', 'authFactory',
 			price: 120,
 			name: "Harry Potter the next gen",
 			orderDate: "12/04/14",
-			status: "Ordered",
-			hasPaid: false
+			status: "Ordered"
 		},
 		{
 			price: 325,
 			name: "Punpun and friends",
 			orderDate: "12/04/14",
-			status: "Confirmed",
-			hasPaid: true
-		}]
+			status: "Confirmed"
+		}];
+		$scope.activeContents = [{
+			price: 120,
+			name: "Harry Potter the next gen",
+			orderDate: "12/04/14",
+			status: "Delivering"
+		},
+		{
+			price: 325,
+			name: "Punpun and friends",
+			orderDate: "12/04/14",
+			status: "Received"
+		},
+		{
+			price: 114,
+			name: "No one has returned",
+			orderDate: "11/03/13",
+			status: "Returning"
+		}];
+		$scope.endedContents = [{
+			price: 120,
+			name: "Harry Potter the next gen",
+			orderDate: "12/04/14",
+			status: "Ended"
+		},
+		{
+			price: 325,
+			name: "Punpun and friends",
+			orderDate: "12/04/14",
+			status: "Ended"
+		}];
 	}
 ]);
