e554ef5e37b06a6d04449dd3257f282bb42aeb4e
Mintra <mntrqq@gmail.com>
2015-11-18 19:09:25 +0700

    complete view profile test cases

--- test/protractor/viewIndividualBook/viewIndividualBookSpec.js
+++ test/protractor/viewIndividualBook/viewIndividualBookSpec.js
@@ -1,8 +1,21 @@
 describe('View Individual Book', function() {
     
+    var books = [];
+    var titleHeader = element(by.id('title-header'));
+
 
     beforeEach(function() {
         browser.get('/#/');
+    });
+
+    beforeAll(function() {
+        $http.get('https://bookieservice.herokuapp.com/api/books')
+            .success(function(data) {
+                books = data.books;
+            })
+            .error(function(data) {
+                console.log(data);
+            });
     });
 
     var urlChanged = function(url) {
@@ -13,12 +26,44 @@ describe('View Individual Book', function() {
         };
     };
 
-    describe(' cases',function() {
-        it('test', function() {
+    describe('case',function() {
+        it('should match title', function() {
             expect(browser.getTitle()).toEqual('Home');
         });
 
+        it('info', function() {
+            expect(titleHeader).toEqual(books[0].title);
+        });
+
     });
+
+    function httpGet(https://bookieservice.herokuapp.com/api/books) {
+        var http = require('http');
+        var defer = protractor.promise.defer();
+
+        http.get("https://bookieservice.herokuapp.com/api/books", function(data) {
+
+            var bodyString = '';
+
+            data.setEncoding('utf8');
+
+            data.on("data", function(chunk) {
+                bodyString += chunk;
+            });
+
+            data.on('end', function() {
+                defer.fulfill({
+                    statusCode: data.statusCode,
+                    bodyString: bodyString
+                });
+            });
+
+        }).on('error', function(e) {
+                defer.reject("Got http.get error: " + e.message);
+        });
+
+        return defer.promise;
+    };
 
 
     afterAll(function(done) {
