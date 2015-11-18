c4d8294ca67d1531c05b63650b6edfbf6cb68cab
rookiemo <frozenprince.k@gmail.com>
2015-11-18 16:50:41 +0700

    link with api

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -578,6 +578,40 @@ app.controller('navCtrl', ['$scope', '$http', '$state', 'authFactory', '$rootSco
 		});
 }]);
 
+app.controller('paymentCtrl',['$scope','$http', '$state', 'authFactory',
+    function ($scope, $http, $state, authFactory){
+        var config = {
+            headers: {
+                'Authorization': authFactory.getAuth()
+            }
+        };
+
+        $scope.getTotal = function() {
+            $scope.total = 0;
+            for(var i = 0, len = $scope.stocks.length; i < len; i++) {
+                $scope.total += $scope.stocks[i].price;
+            }
+        };
+
+        $scope.getCart = function() {
+            $http.get('https://bookieservice.herokuapp.com/api/members/cart/show',config)
+            .success(function (data) {
+                console.log(data);
+                $scope.cart = data;
+                $scope.stocks = $scope.cart.stocks;
+                $scope.getTotal();
+                $scope.countStocks();
+                console.log($scope.total);
+            })
+            .error(function (data) {
+                console.log(data);
+            });
+        };
+
+        //initialize
+        $scope.getCart();
+}]);
+
 app.controller('registerCtrl', ['$scope', '$http', 'mapFactory', '$state', 'authFactory', 'dateFactory',
         function ($scope, $http, $map, $state, authFactory, $date) {
 		if (authFactory.getAuth() !== undefined) {
