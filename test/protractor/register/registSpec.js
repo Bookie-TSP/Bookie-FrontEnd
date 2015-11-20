03cbe7979683bc1856585ed21bee6205e5282626
naneen <neen_tta@hotmail.com>
2015-11-20 21:28:10 +0700

    tested all test cases of registeration page

--- test/protractor/register/registSpec.js
+++ test/protractor/register/registSpec.js
@@ -2,7 +2,7 @@ describe('Register', function() {
     var email = element(by.model('email'));
     var password = element(by.model('password'));
     var submit = element(by.css('[ng-click="submit()"]'));
-    var errormsg = element(by.binding('validation'));
+    var passwordMsg = element(by.id('validate'));
     var navEmail = element(by.binding('member.email'));
     var confPass = element(by.model('password_confirmation'));
     var firstname = element(by.model('first_name'));
@@ -35,30 +35,84 @@ describe('Register', function() {
         });
     });
 
-    describe('email must be alphabet or number and follows email format', function() {
-        it('should not pass with incompleted email', function() {
-            email.sendKeys('bookie');
-            password.sendKeys('password');
-            confPass.sendKeys('password');
-            firstname.sendKeys('test');
-            lastname.sendKeys('test');
-            id.sendKeys('1111111111111');
-            date.element(by.cssContainingText('option', '31')).click();
-            month.element(by.cssContainingText('option', 'January')).click();
-            year.element(by.cssContainingText('option', '1995')).click();
-            gender.get(0).click();
-            phone.sendKeys('0814224100');
-            browser.pause();
-            agree.click();
+    describe('required field', function() {
+        it('must not be blank', function() {
             submit.click();
-            isAlert('error : [object Object]');
+            // wait up to 1000ms for alert to pop up.
+            browser.wait(protractor.ExpectedConditions.alertIsPresent(), 1000); 
+            var alertDialog = browser.switchTo().alert();
+            expect(alertDialog.getText()).toEqual('Please agree the term of condition');
+            alertDialog.accept();
             expect(browser.getCurrentUrl()).toEqual('http://localhost:8000/#/register', 5000);
         });
+    });
 
-        // it('shouldn''t pass with invalid email (not email format)', function() {
-        //     email.sendKeys('@@@@@@@.com');
-        //     password.sendKeys('password');
-        //     confPass.sendKeys('password');
+    // describe('email', function() {
+    //     // it('should not pass with worng format', function() {
+    //     //     // browser.refresh();
+    //     //     email.sendKeys('@@@@@@@.com');
+    //     //     password.sendKeys('password');
+    //     //     confPass.sendKeys('password');
+    //     //     firstname.sendKeys('test');
+    //     //     lastname.sendKeys('test');
+    //     //     id.sendKeys('1111111111111');
+    //     //     date.element(by.cssContainingText('option', '31')).click();
+    //     //     month.element(by.cssContainingText('option', 'January')).click();
+    //     //     year.element(by.cssContainingText('option', '1995')).click();
+    //     //     gender.get(0).click();
+    //     //     phone.sendKeys('0814224100');
+    //     //     browser.pause();
+    //     //     agree.click();
+    //     //     submit.click();
+    //     //     isAlert('error : [object Object]');
+    //     //     expect(browser.getCurrentUrl()).toEqual('http://localhost:8000/#/register', 5000);
+    //     // });
+
+    //     // it('should not pass with incompleted email', function() {
+    //     //     email.sendKeys('bookie');
+    //     //     password.sendKeys('password');
+    //     //     confPass.sendKeys('password');
+    //     //     firstname.sendKeys('test');
+    //     //     lastname.sendKeys('test');
+    //     //     id.sendKeys('1111111111111');
+    //     //     date.element(by.cssContainingText('option', '31')).click();
+    //     //     month.element(by.cssContainingText('option', 'January')).click();
+    //     //     year.element(by.cssContainingText('option', '1995')).click();
+    //     //     gender.get(0).click();
+    //     //     phone.sendKeys('0814224100');
+    //     //     browser.pause();
+    //     //     agree.click();
+    //     //     submit.click();
+    //     //     isAlert('error : [object Object]');
+    //     //     expect(browser.getCurrentUrl()).toEqual('http://localhost:8000/#/register', 5000);
+    //     // });
+
+    //     it('should pass with valid email', function() {
+    //         email.sendKeys('3333@ku.th');
+    //         password.sendKeys('password');
+    //         confPass.sendKeys('password');
+    //         firstname.sendKeys('test');
+    //         lastname.sendKeys('test');
+    //         id.sendKeys('1111111111111');
+    //         date.element(by.cssContainingText('option', '31')).click();
+    //         month.element(by.cssContainingText('option', 'January')).click();
+    //         year.element(by.cssContainingText('option', '1995')).click();
+    //         gender.get(0).click();
+    //         phone.sendKeys('0814224100');
+    //         browser.pause();
+    //         agree.click();
+    //         submit.click();
+    //         // isAlert('error : [object Object]');
+    //         expect(browser.getCurrentUrl()).toEqual('http://localhost:8000/#/login', 5000);
+    //     });
+    // });
+
+    describe('length of Password', function() {
+        // it('should not pass with more than 72 characters', function() {
+        //     // browser.refresh();
+        //     email.sendKeys('3333.com');
+        //     password.sendKeys('1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890');
+        //     confPass.sendKeys('1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890');
         //     firstname.sendKeys('test');
         //     lastname.sendKeys('test');
         //     id.sendKeys('1111111111111');
@@ -74,33 +128,34 @@ describe('Register', function() {
         //     expect(browser.getCurrentUrl()).toEqual('http://localhost:8000/#/register', 5000);
         // });
 
-        // it('should pass with valid email', function() {
-        //     email.sendKeys('1111@a.com');
-        //     password.sendKeys('password');
-        //     confPass.sendKeys('password');
-        //     firstname.sendKeys('test');
-        //     lastname.sendKeys('test');
-        //     id.sendKeys('1111111111111');
-        //     date.element(by.cssContainingText('option', '31')).click();
-        //     month.element(by.cssContainingText('option', 'January')).click();
-        //     year.element(by.cssContainingText('option', '1995')).click();
-        //     gender.get(0).click();
-        //     phone.sendKeys('0814224100');
-        //     browser.pause();
-        //     agree.click();
-        //     submit.click();
-        // });
+        it('should not pass with less than 8 characters', function() {
+            // browser.refresh();
+            email.sendKeys('3333.com');
+            password.sendKeys('123');
+            confPass.sendKeys('123');
+            passwordMsg.toEqual('Not enough characters');
+            firstname.sendKeys('test');
+            lastname.sendKeys('test');
+            id.sendKeys('1111111111111');
+            date.element(by.cssContainingText('option', '31')).click();
+            month.element(by.cssContainingText('option', 'January')).click();
+            year.element(by.cssContainingText('option', '1995')).click();
+            gender.get(0).click();
+            phone.sendKeys('0814224100');
+            browser.pause();
+            agree.click();
+            submit.click();
+            isAlert('error : [object Object]');
+            expect(browser.getCurrentUrl()).toEqual('http://localhost:8000/#/register', 5000);
+        });
+    });
 
-        // it('should not be blank', function() {
-        //     submit.click();
-        //     // wait up to 1000ms for alert to pop up.
-        //     browser.wait(protractor.ExpectedConditions.alertIsPresent(), 1000); 
-        //     var alertDialog = browser.switchTo().alert();
-        //     expect(alertDialog.getText()).toEqual('Please agree the term of condition');
-        //     alertDialog.accept();
-        //     expect(browser.getCurrentUrl()).toEqual('http://localhost:8000/#/register', 5000);
-        // });
-
+    describe('password checking', function() {
+        it('should show errer message if password not match', function{
+            password.sendKeys('12345678');
+            confPass.sendKeys('87654321');
+            passwordMsg.toEqual('Not enough characters');
+        });
     });
 
     afterAll(function(done) {
