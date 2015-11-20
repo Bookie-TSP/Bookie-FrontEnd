ccfce96b1e0967e8ac7e7a12aa90efe3490614e7
tan_dooky <tan_dooky@windowslive.com>
2015-11-20 08:27:54 +0700

    change jquery to angular

--- app/scripts/controllers/searchStock.js
+++ app/scripts/controllers/searchStock.js
@@ -1,7 +1,7 @@
 app.controller('searchStockCtrl', ['$scope', '$http', '$state', '$rootScope', 'dateFactory',
     function ($scope, $http, $state, $rootScope, $date) {
         // amount of books from api
-        $scope.numBooks = -1;
+        $scope.totalBooks = -1;
 
         // language selections
         $scope.langs = ['English', 'Thai', 'Japanese', 'Chinese'];
@@ -9,181 +9,56 @@ app.controller('searchStockCtrl', ['$scope', '$http', '$state', '$rootScope', 'd
         // date
         $scope.initDates = $date.days;
         $scope.initMonths = $date.months;
-        $scope.initYears = $date.years; 
+        $scope.initYears = $date.years;
 
-		var maxResults = 10;
+        //books
+        $scope.results = [];
+
+        var maxResults = 10;
 		var startIndex = 0;
 		var totalBooks = 0;
 		var apiKey = "AIzaSyAY-BLl9HgepqEFBxR5YJbC_qdE4PZF_6g";
-		var searchKey, operation;
 
-		var hideButton = function (button) {
-			button.toggle(false);
-		};
+        $scope.hasNext = function(){
+            return (startIndex + maxResults <= $scope.totalBooks);
+        };
 
-		var showButton = function (button) {
-			button.toggle(true);
-		};
+        $scope.hasPrevious = function(){
+            console.log(startIndex);
+            return startIndex !== 0;
+        };
 
-		var buttonToggle = function () {
-			console.log(startIndex);
-
-			var previousButton = $('#previous-page');
-			var nextButton = $('#next-page');
-
-			//previous button
-			if (startIndex === 0) {
-				hideButton(previousButton);
-			} else {
-				showButton(previousButton);
-			}
-			//next button
-			if (startIndex + maxResults > totalBooks) {
-				hideButton(nextButton);
-			} else {
-				showButton(nextButton);
-			}
-		};
-
-		var htmlConvert = function (thumbnail, title, publisher, isbn) {
-			var htmlTemplate = "<div class=\"col-lg-2 col-md-3 col-xs-4\" style=\"" +
-				"margin-top: 12px;" +
-				"margin-bottom: 0px;" +
-				"margin-right: 0px;" +
-				"margin-left: 12px;" +
-
-				"padding-top: 20px;" +
-				"padding-bottom: 20px;" +
-				"padding-right: 20px;" +
-				"padding-left: 20px;" +
-
-				"background-color: #fcf8e3;" +
-				"border-color: #faebcc;" +
-
-				"height: 348px;\">" +
-
-				"<div class=\"book-info\" >" +
-				//Image
-				"<div class=\"thumbnail\" style=\"height: 200px;\" ><img src=\"" + thumbnail + "\"></div>" +
-
-				//Book Title Box
-				"<div class=\"title\" style=\"" +
-				//property
-				"display: -webkit-box;" +
-				"-webkit-line-clamp: 3;" +
-				"-webkit-box-orient: vertical;" +
-				"overflow: hidden;" +
-				"text-overflow:ellipsis;" +
-				"max-height: 60px;" +
-				"min-height: 20px;\">" +
-				//text
-				"<b>" + title + "</b>" + "</div>" +
-
-				//Book Author Box
-				"<div class=\"publisher\" style=\"" +
-				//property
-				"overflow: hidden;" +
-				"max-height: 40px;" +
-				"min-height: 20px;\">" +
-				//text
-				"<em>" + publisher + "</em>" + "</div>" +
-				"</div>" +
-				"</div>";
-
-			return htmlTemplate;
-		};
-
-		var showResult = function (data) {
-			buttonToggle();
-			var books = data.items;
-            console.log($scope.numBooks);
-			// no books || no matched items
-			if (totalBooks === 0) {
-				$('#result')
-					.append("No book found from database");
-			} else {
-				var i, j, book, thumbnail, title, publisher, isbn;
-				for (i = 0; i < books.length; i++) {
-					book = books[i];
-					console.log(book);
-					// no thumbnail
-					if (book.volumeInfo.imageLinks === undefined) {
-						// this is "not available" image
-						thumbnail = "http://webmaster.ypsa.org/wp-content/uploads/2012/08/no_thumb.jpg";
-					} else {
-						thumbnail = book.volumeInfo.imageLinks.smallThumbnail;
-					}
-
-					title = book.volumeInfo.title;
-
-					// no isbn
-					if (book.volumeInfo.industryIdentifiers === undefined) {
-						isbn = "not available";
-					} else {
-						isbn = book.volumeInfo.industryIdentifiers[0].identifier;
-					}
-
-					// no publisher
-					if (book.volumeInfo.publisher === undefined) {
-						publisher = "not available";
-					} else {
-						publisher = book.volumeInfo.publisher;
-					}
-
-					var html = htmlConvert(thumbnail, title, publisher, isbn);
-					$('#result')
-						.append(html);
-				}
-			}
-		};
-
-		var clearResult = function () {
-			$('#result')
-				.html("");
-		};
-
-		var getBooks = function (searchKey, operation, startIndex, maxResults, apiKey) {
+		$scope.getBooks = function (searchKey, operation, startIndex, maxResults, apiKey) {
 			$http.get("https://www.googleapis.com/books/v1/volumes?q=" + operation + ":" + searchKey +
 				"&maxResults=" + maxResults + "&startIndex=" + startIndex +
 				"&key=" + apiKey)
                 .success(function (data) {
+                    console.log("https://www.googleapis.com/books/v1/volumes?q=" + operation + ":" + searchKey +
+        				"&maxResults=" + maxResults + "&startIndex=" + startIndex +
+        				"&key=" + apiKey);
 					//data is the matched items that returned from Google books API
-					totalBooks = data.totalItems;
-                    $scope.numBooks = totalBooks;
-                    console.log($scope.numBooks);
-					showResult(data);
-				});
+					//totalBooks = data.totalItems;
+                    console.log(data);
+                    $scope.results = data.items;
+                    $scope.totalBooks = data.totalItems;
+					//showResult(data);
+				})
+                .error(function(data){
+                    console.log(data);
+                });
 		};
 
-		$('#search-button')
-			.click(function () {
-				clearResult();
-				// if the search button is clicked, then show the pagination buttons
-				$('.pagination')
-					.each(function () {
-						$(this)
-							.attr("style", "");
-					});
-				searchKey = $('#search-field')
-					.val();
-				operation = $('input:checked')
-					.attr("value");
-				getBooks(searchKey, operation, startIndex, maxResults, apiKey);
-			});
+        $scope.search = function(){
+            $scope.getBooks($scope.searchField, $scope.searchCat, startIndex, maxResults, apiKey);
+        };
 
-		//manual pagination
-		$('#next-page')
-			.click(function () {
-				clearResult();
-				startIndex += maxResults - 1;
-				getBooks(searchKey, operation, startIndex, maxResults, apiKey);
-			});
+        $scope.nextPage = function() {
+            startIndex += maxResults;
+            $scope.getBooks($scope.searchField, $scope.searchCat, startIndex, maxResults, apiKey);
+        };
 
-		//manual pagination
-		$('#previous-page')
-			.click(function () {
-				clearResult();
-				startIndex -= maxResults - 1;
-				getBooks(searchKey, operation, startIndex, maxResults, apiKey);
-			});
+        $scope.previousPage = function() {
+            startIndex -= maxResults;
+            $scope.getBooks($scope.searchField, $scope.searchCat, startIndex, maxResults, apiKey);
+        };
 }]);
