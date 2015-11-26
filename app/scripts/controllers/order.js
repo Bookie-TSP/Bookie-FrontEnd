0890dcd6dde9eca47e341b92f6fc6ef40b7941b3
Varis <win3455@hotmail.com>
2015-11-26 10:43:37 +0700

    Probably done, still wating for mock data to test

--- app/scripts/controllers/order.js
+++ app/scripts/controllers/order.js
@@ -19,49 +19,7 @@ app.controller('orderCtrl', ['$scope', '$http', '$state', 'authFactory',
 					console.log(data);
 				});
 		}
-
-		$scope.pendingContents = [{
-			price: 120,
-			name: "Harry Potter the next gen",
-			orderDate: "12/04/14",
-			status: "Ordered"
-		},
-		{
-			price: 325,
-			name: "Punpun and friends",
-			orderDate: "12/04/14",
-			status: "Confirmed"
-		}];
-		$scope.activeContents = [{
-			price: 120,
-			name: "Harry Potter the next gen",
-			orderDate: "12/04/14",
-			status: "Delivering"
-		},
-		{
-			price: 325,
-			name: "Punpun and friends",
-			orderDate: "12/04/14",
-			status: "Received"
-		},
-		{
-			price: 114,
-			name: "No one has returned",
-			orderDate: "11/03/13",
-			status: "Returning"
-		}];
-		$scope.endedContents = [{
-			price: 120,
-			name: "Harry Potter the next gen",
-			orderDate: "12/04/14",
-			status: "Ended"
-		},
-		{
-			price: 325,
-			name: "Punpun and friends",
-			orderDate: "12/04/14",
-			status: "Ended"
-		}];
+		
 		$scope.getOrderInfo();
 	}
 ]);
