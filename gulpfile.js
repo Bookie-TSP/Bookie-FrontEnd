1ca2b25249f49f4d4e1a3066f47bdc340c7ab596
tan_dooky <tan_dooky@windowslive.com>
2015-12-03 10:01:33 +0700

    change all to bower

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,6 @@ gulp.task('libaryScripts', function () {
 						'./bower_components/jquery/dist/jquery.min.js',
 						'./bower_components/jquery-ui/jquery-ui.min.js',
 						'./bower_components/jQuery.dotdotdot/src/js/jquery.dotdotdot.min.js',
-						'./bower_components/angular-bootstrap/ui-bootstrap.min.js',
 						'./bower_components/angular-google-maps/dist/angular-google-maps.min.js',
 						'./bower_components/angular-simple-logger/dist/angular-simple-logger.min.js',
 						'./bower_components/loadash/loadash.min.js',
@@ -31,9 +30,7 @@ gulp.task('libaryScripts', function () {
 						'./bower_components/ng-file-upload/ng-file-upload-shim.js',
 						'./bower_components/ng-file-upload/ng-file-upload.js',
 						'./bower_components/cloudinary_ng/js/angular.cloudinary.js',
-						'./bower_components/cloudinary-core/cloudinary-core.js',
-						'./bower_components/cloudinary-jquery/cloudinary-jquery.js',
-						'./bower_components/cloudinary-jquery-file-upload/cloudinary-jquery-file-upload.js'
+						'./bower_components/cloudinary_js/js/jquery.cloudinary.js'
 					])
 		.pipe(concat('libary.js'))
 		.pipe(gulp.dest('app/scripts'));
